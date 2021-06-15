393467cd744a5de4de1cc9eb0d3909e2b9e04893
Benjamin Groseclose <ben.groseclose01@gmail.com>
2021-06-14 23:27:42 -0400

    setup app

--- App.js
+++ App.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { StyleSheet, TouchableWithoutFeedback, Keyboard } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './app/screens/home';
 import GarageSaleMap from './app/screens/garage-sale-map';
 import CreateGarageSale from './app/screens/create-garage-sale';
 
-export default function App() {
+const App = () => {
 
   const Stack = createStackNavigator();
   
   const theme = {
+    ...DefaultTheme,
     dark: false,
-
-  }
+    colors: {
+      ...DefaultTheme.colors,
+      primary: '#EEE',
+      background: '#72A0C1'
+    }
+  };
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <NavigationContainer>
+      <NavigationContainer theme={theme}>
         <Stack.Navigator>
           <Stack.Screen name="Home" component={Home} />
           <Stack.Screen name="Maps" component={GarageSaleMap} />
